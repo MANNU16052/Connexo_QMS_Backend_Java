36f45b92bf3dd777c4e4c8d482ecf1e008898d8e
manish <mannumalviya16052@gmail.com>
2023-12-25 14:45:12 +0530

    QMS_Project_Setup

---
+++ QMS/src/test/java/QMS_Java_Backend/QmsApplicationTests.java
@@ -0,0 +1,13 @@
+package QMS_Java_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
