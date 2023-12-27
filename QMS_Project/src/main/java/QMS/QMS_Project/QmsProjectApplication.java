556d1c6bdc88d984a9aae26be9f36f6996c7e95f
manish <mannumalviya16052@gmail.com>
2023-12-27 12:56:08 +0530

    QMS_Project_Setup

---
+++ QMS_Project/src/main/java/QMS/QMS_Project/QmsProjectApplication.java
@@ -0,0 +1,13 @@
+package QMS.QMS_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QmsProjectApplication.class, args);
+	}
+
+}
