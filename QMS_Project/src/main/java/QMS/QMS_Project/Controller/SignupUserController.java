fe3a708524e4a2fc4ebb6c53a35948bfd1f6bd66
Serveradminai <shivampatel2859@gmail.com>
2024-01-16 14:29:48 +0530

    change signup

--- QMS_Project/src/main/java/QMS/QMS_Project/Controller/SignupUserController.java
+++ QMS_Project/src/main/java/QMS/QMS_Project/Controller/SignupUserController.java
@@ -1,7 +1,7 @@
 package QMS.QMS_Project.Controller;
 import QMS.QMS_Project.dtos.SignupRequest;
 import QMS.QMS_Project.dtos.UserDTO;
-
+import QMS.QMS_Project.model.User;
 import QMS.QMS_Project.service.AuthService;
 
 import jakarta.servlet.http.HttpSession;
@@ -18,11 +18,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+ import org.springframework.web.bind.annotation.RestController;
 public class SignupUserController{
-	
     @Autowired
     private AuthService authService;
- 
+    
 
+    @PostMapping("/register")
+    public ResponseEntity<?> craeteUser(@RequestBody SignupRequest singnRequest){
+        UserDTO createdUser = authService.createUser(singnRequest);
+        if(createdUser == null)
+            return new ResponseEntity<>("User is not Created , try again later", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(createdUser,HttpStatus.CREATED);
+    }
 }
+    
+
